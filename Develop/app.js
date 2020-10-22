3cf30c6b849f61b19148f937381cd832c8e33b7a
rez <benjaminresnicoff@icloud.com>
2020-10-21 23:34:34 -0500

    finished app part of hmwk

--- Develop/app.js
+++ Develop/app.js
@@ -13,6 +13,130 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const teamMember = [];
+function app(){
+    function getManager(){
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the manager's name?",
+                name: "managerName"
+            },
+            {
+                type: "input",
+                message: "What is the manager's id?",
+                name: "managerId"
+            },
+            {
+                type: "input",
+                message: "What is the manager's email",
+                name: "managerEmail",
+            },
+            {   
+                type: "input",
+                message: "What is the manager's office number?",
+                name: "officeNumber",
+            }
+        ]).then(function(response) {
+            const manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.officeNumber)
+            teamMember.push(manager)
+            addingNewMember()
+        })
+    }
+    function getEngineer(){
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the engineer's name?",
+                name: "engineerName"
+            },
+            {
+                type: "input",
+                message: "What is the engineer's id?",
+                name: "engineerId"
+            },
+            {
+                type: "input",
+                message: "What is the engineer's email",
+                name: "engineerEmail",
+            },
+            {   
+                type: "input",
+                message: "What is the github username?",
+                name: "github",
+            }
+        ]).then(function(response) {
+            const engineer = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.github)
+            teamMember.push(engineer)
+            addingNewMember()
+        })
+    }
+    function getIntern(){
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the intern's name?",
+                name: "internName"
+            },
+            {
+                type: "input",
+                message: "What is the intern's id?",
+                name: "internId"
+            },
+            {
+                type: "input",
+                message: "What is the intern's email",
+                name: "internEmail",
+            },
+            {   
+                type: "input",
+                message: "What is the intern's school?",
+                name: "school",
+            }
+        ]).then(function(response) {
+            const intern = new Intern(response.internName, response.internId, response.internEmail, response.school)
+            teamMember.push(intern)
+            addingNewMember()
+        })
+    }
+    function addingNewMember(){
+        inquirer.prompt([
+            {
+                type: "checkbox",
+                name: "selectEmployees",
+                message: "Which employee?",
+                choices: [
+                    "engineer",
+                    "intern",
+                    "manager",
+                    "done",
+                ]
+            }
+        ]).then(function(response) {
+            const role = response.selectEmployees
+            if (role == "manager"){
+                getManager()
+            }
+            else if (role == "engineer"){
+                getEngineer()
+            }
+            else if (role == "intern"){
+                getIntern()
+            }
+            else if (role == "done"){
+                renderTeam()
+            }
+        })
+    }
+    addingNewMember()
+}
+function renderTeam(){
+    fs.writeFileSync(outputPath, render(teamMember), "utf-8")
+}
+app();
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
